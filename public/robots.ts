b4422bcbe5780339c66b84a0c54bd872226cf99f
1mjCee <cjmatheka@gmail.com>
2025-12-19 14:37:47 +0300

    Update seo

--- public/robots.ts
+++ public/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/api/', '/admin/', '/private/'],
       },
     ],
-    sitemap: 'https://prepzi.com/sitemap.xml',
+    sitemap: 'https://prepzi.app/sitemap.xml',
   }
 }
